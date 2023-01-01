6e6e8d39343680d576380a5db9fd61374dcb5858
karasawa <pain.keita.27@gmail.com>
2023-01-02 08:34:12 +0900

    initial commit

--- backend/app/local_settings.py
+++
@@ -1,4 +0,0 @@
-LOCAL_SECRET_KEY = 'django-insecure-@dnl*#-omrdmu--jyxuo&_+$yij70wpv@s_!_(l%a!nfvdd!3+'
-LOCAL_DEBUG = True
-LOCAL_CORS_ORIGIN_WHITEURL1 = 'http://localhost:3000'
-LOCAL_CORS_ORIGIN_WHITEURL2 = 'http://localhost'
